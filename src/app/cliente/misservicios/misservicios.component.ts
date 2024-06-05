2d96bd80f99ea9583a8bfcb7d3e46e6f267342a3
spuchades95 <spuchades95@gmail.com>
2024-06-05 14:44:25 +0200

    cambios en ficha

--- src/app/cliente/misservicios/misservicios.component.ts
+++ src/app/cliente/misservicios/misservicios.component.ts
@@ -18,6 +18,15 @@ export class MisserviciosComponent implements OnInit {
       .subscribe((response: any) => {
         this.servicios = response.data;
         this.tieneServicios = this.servicios.length > 0;
+        this.servicios.sort((a, b) => {
+          const [dayA, monthA, yearA] = a.FechaContratacion.split('/');
+          const [dayB, monthB, yearB] = b.FechaContratacion.split('/');
+          const dateA = `${yearA}${monthA.padStart(2, '0')}${dayA.padStart(2, '0')}`;
+          const dateB = `${yearB}${monthB.padStart(2, '0')}${dayB.padStart(2, '0')}`;
+          return dateB.localeCompare(dateA);
+        });
+    
+        console.log(this.servicios);
       });
   }
 
